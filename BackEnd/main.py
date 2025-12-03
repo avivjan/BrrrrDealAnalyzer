4a1ff9bb9e8f96846d26d350164b61bc5a95892d
Aviv jan <avivjan2000@gmail.com>
2025-12-03 23:32:42 +0200

    Update main.py

--- BackEnd/main.py
+++ BackEnd/main.py
@@ -17,7 +17,11 @@ Base.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://localhost:3000",
+        "https://bigwhalescalculatorfront.netlify.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
